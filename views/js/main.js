d13c1cb9e7cae516bad5b68bb91647d5a48ea295
Prajwal Kumar <prajwalkumar@Prajwals-MacBook-Pro.local>
2016-12-10 07:17:05 +0530

    fix: minor optimization fixes

--- views/js/main.js
+++ views/js/main.js
@@ -455,8 +455,8 @@ var resizePizzas = function(size) {
 window.performance.mark("mark_start_generating"); // collect timing data
 
 // This for-loop actually creates and appends all of the pizzas when the page loads
-for (var i = 2; i < 100; i++) {
   var pizzasDiv = document.getElementById("randomPizzas");
+for (var i = 2; i < 100; i++) {
   pizzasDiv.appendChild(pizzaElementGenerator(i));
 }
 
@@ -488,7 +488,7 @@ function updatePositions() {
   frame++;
   window.performance.mark("mark_start_frame");
 
-  var items = document.querySelectorAll('.mover');
+  var items = document.getElementsByClassName('.mover');
   var temp = document.body.scrollTop / 1250;
   for (var i = 0; i < items.length; i++) {
     var phase = Math.sin(temp + (i % 5));
